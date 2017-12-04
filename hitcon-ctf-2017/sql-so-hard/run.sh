eb006ca52d033e1c9c4d1a8dbc5b813c36b44ad5
Pr0phet <zejunprophet@gmail.com>
2017-12-04 15:30:33 +0800

    [*] try another start up

--- hitcon-ctf-2017/sql-so-hard/run.sh
+++ hitcon-ctf-2017/sql-so-hard/run.sh
@@ -6,24 +6,37 @@ service mysql start
 service postgresql start
 
 #Init postgresql
-su postgres -c 'psql -c \\i /root/psqlInit.sql'
+su postgres -c 'psql -c \\i /home/ctf/psqlInit.sql'
 
 #Init mysql
-mysql -uroot -pPr0ph3t < /root/mysqlInit.sql
+mysql -uroot -pPr0ph3t < /home/ctf/mysqlInit.sql
 
 #clean sql file
-if [ ! -f \'/root/mysqlInit.sql\' ]; then
-	rm -rf /root/mysqlInit.sql
+if [ ! -f \'/home/ctf/mysqlInit.sql\' ]; then
+	rm -rf /home/ctf/mysqlInit.sql
 fi
-if [ ! -f \'/root/psqlInit.sql\' ]; then
-	rm -rf /root/psqlInit.sql
+if [ ! -f \'/home/ctf/psqlInit.sql\' ]; then
+	rm -rf /home/ctf/psqlInit.sql
 fi
 
-su ctf
-cd ~
-source .bashrc
-cd ~/app
+nvm install 4.6.0
 nvm use 4.6.0
+cd /home/ctf/
+npm install express-generator -g
+express app
+
+#Init app env
+cd /home/ctf/app/
+npm install express
+npm install pg@7.0.2
+npm install mysql
+npm install qs
+mv /home/ctf/app.js /home/ctf/app/
+touch /home/ctf/app/nohup.out
+chown ctf /home/ctf/app/nohup.out
+chmod 755 /home/ctf/app/nohup.out
+
+su ctf
 nohup node app.js &
 
 /bin/bash
